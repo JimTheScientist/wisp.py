4dc5140a29d9d1762eb8efcc5f0c68a71c095f83
AidanP <aidannpratt@gmail.com>
2021-04-12 20:54:14 -0400

    made new version

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JimTheScientist/wisp.py",
+    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     keywords = ['wisp', 'api-wrapper', 'api', 'gaming', 'wrapper'],
     classifiers=[
